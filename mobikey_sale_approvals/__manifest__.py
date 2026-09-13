e25fa89f0fd1196f9f18ff989bb3dd10732e4a76
kiambati-abn <maxwel@abn.consulting>
2026-09-13 12:47:38 +0300

    Show approval status in quotation list

--- mobikey_sale_approvals/__manifest__.py
+++ mobikey_sale_approvals/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Mobikey Sales Approvals',
-    'version': '19.0.1.4.0',
+    'version': '19.0.1.4.1',
     'author': 'Mobikey',
     'license': 'LGPL-3',
     'depends': ['mobikey_crm', 'sale_margin', 'stock_account'],
